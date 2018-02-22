60d61e54af6eb8ccfe4d902bedfcc2d1f8c59e70
Lan Phan <lanc_phan@hotmail.com>
2018-02-22 14:23:18 -0500

    change the background colour with the click button

--- randomBackground/js/colorchange.js
+++ randomBackground/js/colorchange.js
@@ -1,5 +1,24 @@
-document.addEventListener('DOMcontentLoaded', randomColour);
+// SOLUTION #1 - RAW JAVASCRIPT WITH ADDEVENTLISTENER ////////////
+document.addEventListener("DOMContentLoaded", function() {
 
-function randomColour() {
+  var button = document.getElementById('button');
+  var body = document.querySelector('body');
 
-};
+  button.addEventListener('click',function() {
+    var newColor = '#' + Math.random().toString(16).slice(2,8);
+    return body.style.background = newColor;
+  });
+});
+
+/////// SOLUTION #2 - JQUERY METHOD /////////////
+
+// $(document).ready(function () {
+//
+// 	function randomColor() {
+// 		return '#' + Math.random().toString(16).slice(2, 8);
+// 	};
+//
+// 	$("#button").on("click", function () {
+// 		$('body').css('background', randomColor());
+// 	});
+// });
