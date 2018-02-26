2c4f6b6c0331b5f9619695efc1ca41abc886fd5e
Lan Phan <lanc_phan@hotmail.com>
2018-02-26 16:52:49 -0500

    add click event handler to start counting down, hide the result and show the fists

--- 03-rockPaperScissor/js/rockpaperscissor.js
+++ 03-rockPaperScissor/js/rockpaperscissor.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+  // DECLARE VARIABLES
   var rock = "images/rock.png";
 	var paper = "images/paper.png";
 	var scissors = "images/scissor.png";
@@ -10,5 +11,24 @@ document.addEventListener('DOMContentLoaded', function() {
   };
 	var choices = ['rock', 'paper', 'scissor'];
 	var userChoice, compChoice;
+
+  // CLICK BUTTON
+  var button = document.getElementsByClassName('button');
+  button.addEventListener('click', start);
+
+	function start() {
+    var button = document.getElementsByClassName('button');
+    button.addEventListener('unclick', function() {
+
+      userChoice = this.id;
+      compChoice = choices[Math.floor(Math.random()*3)];
+
+      var result = document.getElementsByClassName('result');
+      var fist = document.getElementsByClassName('fist');
+
+      result.hide();
+      fist.show();
+    });
+	};
 
 });
