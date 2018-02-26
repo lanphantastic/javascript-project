1c4fd008316545667a616a6a3a17070fc4332c03
Lan Phan <lanc_phan@hotmail.com>
2018-02-26 16:44:11 -0500

    declare variables for each choice

---
+++ 03-rockPaperScissor/js/rockpaperscissor.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var rock = "images/rock.png";
+	var paper = "images/paper.png";
+	var scissors = "images/scissor.png";
+	var hands = {
+    rock: rock,
+    paper: paper,
+    scissor: scissor
+  };
+	var choices = ['rock', 'paper', 'scissor'];
+	var userChoice, compChoice;
+
+});
