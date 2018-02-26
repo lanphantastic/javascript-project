3a0ded40b43a86c86791c31a82a44a108b257d6d
Lan Phan <lanc_phan@hotmail.com>
2018-02-26 15:32:43 -0500

    load DOMContentLoaded with addEventListener and test the 'click' event handler

---
+++ 02-titleGenerator/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  var kwSubmit = document.getElementById('kw-submit');
+
+  kwSubmit.addEventListener('click', function() {
+    alert('hi. this is a test');
+  });
+
+});
