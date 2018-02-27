7d6f114578b5457c57a2b36c8776f21191a1d08e
Lan Phan <lanc_phan@hotmail.com>
2018-02-27 13:38:00 -0500

    grab all of the button choices and check if it outputs rock, paper or scissors

--- 03-rockPaperScissor/js/rockpaperscissor.js
+++ 03-rockPaperScissor/js/rockpaperscissor.js
@@ -1,34 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded',function() {
 
   // DECLARE VARIABLES
-  var rock = "images/rock.png";
+	var rock = "images/rock.png";
 	var paper = "images/paper.png";
-	var scissors = "images/scissor.png";
+	var scissors = "images/scissors.png";
 	var hands = {
     rock: rock,
     paper: paper,
-    scissor: scissor
+    scissors: scissors
   };
-	var choices = ['rock', 'paper', 'scissor'];
+	var choices = ['rock', 'paper', 'scissors'];
 	var userChoice, compChoice;
 
-  // CLICK BUTTON
+  // MAKE EACH BUTTON CLICKABLE
+  // grab all DOM object with the class 'button'
   var button = document.getElementsByClassName('button');
-  button.addEventListener('click', start);
+  for (var i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', start);
+  }
 
+	// $('.button').on('click', start);
+  //
 	function start() {
-    var button = document.getElementsByClassName('button');
-    button.addEventListener('unclick', function() {
+		userChoice = this.id;
+    console.log(`You chose ${userChoice}`);
+		compChoice = choices[Math.floor(Math.random()*3)];
+    console.log(`computer picked ${compChoice}`);
 
-      userChoice = this.id;
-      compChoice = choices[Math.floor(Math.random()*3)];
+    var result = document.getElementsByClassName('result');
+    var fist = document.getElementsByClassName('fist');
 
-      var result = document.getElementsByClassName('result');
-      var fist = document.getElementsByClassName('fist');
-
-      result.hide();
-      fist.show();
-    });
 	};
 
 });
